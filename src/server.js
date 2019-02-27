a28123a1d305280ff62923fc23ddad202c0b04af
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-02-28 02:32:05 +0530

    extracted rewards points

--- src/server.js
+++ src/server.js
@@ -13,6 +13,21 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.post('/auth', Auth)
 app.get('/users', FetchAllUsers)
 
+app.post('/payload', (request, response) => {
+
+    const payload = request.body
+
+    if (payload.event_type == 'merge_request') {
+        const { user, object_attributes } = payload
+        
+        if ( object_attributes.state === 'merged' ) {
+            const { source_branch } = object_attributes
+            const splittedBranchName = source_branch.split('-')
+            const rewardsPoints = splittedBranchName[1]
+        }
+    }
+})
+
 app.listen(
     PORT, () => console.log('Hurray APIs are live....')
 )
